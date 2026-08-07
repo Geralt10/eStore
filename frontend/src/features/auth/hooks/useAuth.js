c11dce0369d5ee437d32d6e10d61a2f1c21a6f56
himanshu dhoundiyal <105155811+Geralt10@users.noreply.github.com>
2026-08-07 17:12:47 +0530

    updates

--- frontend/src/features/auth/hooks/useAuth.js
+++ frontend/src/features/auth/hooks/useAuth.js
@@ -3,5 +3,25 @@ import { useDispatch } from "react-redux";
 import { setError, setLoading, setUser } from "../state/auth.slice";
 
 export const useAuth = () =>{
-    const di
+    const dispatch = useDispatch();
+
+    const handleRegister = async ({fullname,email,password,contact,isSeller=false}) =>{
+        try{
+            dispatch(setLoading(true))
+            const response = await register({fullname,email,password,contact,isSeller})
+            dispatch(setUser(response.user))
+            dispatch(setError(null))
+            return true
+        }catch(error){
+            dispatch(setError(error.message))
+            dispatch(setUser(null))
+            return false
+        }finally{
+            dispatch(setLoading(false))
+        }
+    }
+
+    return {
+        handleRegister
+    }
 }
